32c0452e822d2fba01bf5cde3a1aa63f10990602
Joe Boyle <jboyle617@gmail.com>
2024-05-13 09:41:10 -0400

    lists

--- app/Main.hs
+++ app/Main.hs
@@ -3,5 +3,16 @@ module Main where
 doubleMe :: Num a => a -> a
 doubleMe x = x + x
 
+doubleUs :: Num a => a -> a -> a
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber :: (Ord a, Num a) => a -> a
+doubleSmallNumber x = if x > 100
+                         then x
+                         else x * 2
+
+lostNumbers :: [Int]
+lostNumbers = [4, 8, 15, 16, 23, 42]
+
 main :: IO ()
-main = print (doubleMe 4 :: Int)
+main = print (lostNumbers !! 4)
