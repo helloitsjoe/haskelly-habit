151301f021e3e4c436967907f03f3acf125cf778
Joe Boyle <jboyle617@gmail.com>
2024-05-13 20:44:18 -0400

    More learning

--- app/Main.hs
+++ app/Main.hs
@@ -14,5 +14,44 @@ doubleSmallNumber x = if x > 100
 lostNumbers :: [Int]
 lostNumbers = [4, 8, 15, 16, 23, 42]
 
+fifthLost :: Int
+fifthLost = lostNumbers !! 4
+
+lostHead :: Int
+lostHead = head lostNumbers
+
+lostTail :: [Int]
+lostTail = tail lostNumbers
+
+lostInit :: [Int]
+lostInit = init lostNumbers
+
+lostLast :: Int
+lostLast = last lostNumbers
+
+lostLen :: Int
+lostLen = length lostNumbers
+
+lostFirstThree :: [Int]
+lostFirstThree = take 3 lostNumbers
+
+lostLastThree :: [Int]
+lostLastThree = drop 3 lostNumbers
+
+lostMin :: Int
+lostMin = minimum lostNumbers
+
+lostProduct :: Int
+lostProduct = product lostNumbers
+
+lostSixteenExists :: Bool
+lostSixteenExists = 16 `elem` lostNumbers
+
+range :: [Int]
+range = [1..20]
+
+rangeByThrees :: [Int]
+rangeByThrees = [3,6..20]
+
 main :: IO ()
-main = print (lostNumbers !! 4)
+main = print rangeByThrees
