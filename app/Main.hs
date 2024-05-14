b177cc88f7bbc8283b6c0c5d2d8cb617cd98d8fa
Joe Boyle <jboyle617@gmail.com>
2024-05-13 22:14:04 -0400

    Simplify types

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,15 @@
 module Main where
 
-doubleMe :: Num a => a -> a
+-- Basic functions
+
+doubleMe :: Int -> Int
 doubleMe x = x + x
 
-doubleUs :: Num a => a -> a -> a
+-- doubleUs :: Num a => a -> a -> a
+doubleUs :: Int -> Int -> Int
 doubleUs x y = doubleMe x + doubleMe y
 
-doubleSmallNumber :: (Ord a, Num a) => a -> a
+doubleSmallNumber :: Int -> Int
 doubleSmallNumber x = if x > 100
                          then x
                          else x * 2
@@ -53,5 +56,40 @@ range = [1..20]
 rangeByThrees :: [Int]
 rangeByThrees = [3,6..20]
 
+firstFiveInfinite :: [Int]
+firstFiveInfinite = take 5 [1..]
+
+firstFiveCycle :: String
+firstFiveCycle = take 5 (cycle "ABC")
+
+firstFiveRepeat :: [Int]
+-- firstFiveRepeat = take 5 (repeat 3)
+firstFiveRepeat = replicate 5 3
+
+-- List comprehensions
+
+firstTenDoubled :: [Int]
+firstTenDoubled = [x*2 | x <- [1..10]]
+
+fiftyToHundredModSevenThree :: [Int]
+fiftyToHundredModSevenThree = [ x | x <- [50..100], x `mod` 7 == 3]
+
+bangBoom :: (Ord xs, Integral xs, Num xs) => [xs] -> [String]
+bangBoom xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+multiplePredicates :: [Int]
+multiplePredicates = [ x | x <- [1..20], x /= 5, x /= 12, x /= 18]
+
+multipleInputLists :: [Int]
+multipleInputLists = [x*y | x <- [2, 4, 5], y <- [3, 6, 9]]
+
+myLength :: [Int] -> Int
+myLength xs = sum [ 1 | _ <- xs ]
+
+nested :: [[Int]]
+nested = [[1..10], [11..20], [21..30]]
+nestedEven :: [[Int]] -> [[Int]]
+nestedEven xxs = [[ x | x <- xs, even x] | xs <- xxs]
+
 main :: IO ()
-main = print rangeByThrees
+main = print (nestedEven nested)
