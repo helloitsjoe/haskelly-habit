4da95e430d8fdb052ddc37627c84628993745680
Joe Boyle <jboyle617@gmail.com>
2024-05-24 18:59:13 -0400

    More higher order fns

--- app/HigherOrder.hs
+++ app/HigherOrder.hs
@@ -6,5 +6,21 @@ zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
 concatChars :: Char -> Char -> String
 concatChars x y = x : [y]
 
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+addThree :: (Num a) => a -> a
+addThree x = x + 3
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' a = b
+  where b x y = a y x
+
+subSecondFromFirst :: (Num a) => a -> a -> a
+subSecondFromFirst x y = x - y
+
+subFirstFromSecond :: (Num a) => a -> a -> a
+subFirstFromSecond = flip' subSecondFromFirst
+
 main :: IO ()
-main = print (zipWith' concatChars ['a','b'] ['c','d'])
+main = print (subFirstFromSecond 2 1)
