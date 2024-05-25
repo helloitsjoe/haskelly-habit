575e52207df20b28a9611ba0a30ace09cfbd3346
Joe Boyle <jboyle617@gmail.com>
2024-05-24 23:05:33 -0400

    collatz

--- app/HigherOrder.hs
+++ app/HigherOrder.hs
@@ -13,8 +13,7 @@ addThree :: (Num a) => a -> a
 addThree x = x + 3
 
 flip' :: (a -> b -> c) -> (b -> a -> c)
-flip' a = b
-  where b x y = a y x
+flip' f x y = f y x
 
 subSecondFromFirst :: (Num a) => a -> a -> a
 subSecondFromFirst x y = x - y
@@ -22,5 +21,29 @@ subSecondFromFirst x y = x - y
 subFirstFromSecond :: (Num a) => a -> a -> a
 subFirstFromSecond = flip' subSecondFromFirst
 
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+  let less = quicksort (filter (<x) xs)
+      more = quicksort (filter (>=x) xs)
+  in less ++ [x] ++ more
+
+largestUnder100kDivisible :: Int
+largestUnder100kDivisible = head (filter p [100000,99999..])
+  where p x = x `mod` 3829 == 0
+
+sumOfOddSquaresSmallerThanTenK :: Int
+sumOfOddSquaresSmallerThanTenK = sum (takeWhile (<10000) (map g [1,3..]))
+  where g x = x ^ 2
+
+collatzChain :: (Integral a) => a -> [a]
+collatzChain 1 = [1]
+collatzChain n
+  | even n = n:collatzChain (n `div` 2)
+  | odd n = n:collatzChain (n * 3 + 1)
+
+chainsGT15 =  length (filter f [1..100])
+  where f x = length (collatzChain x) > 15
+
 main :: IO ()
-main = print (subFirstFromSecond 2 1)
+main = print (chainsGT15)
