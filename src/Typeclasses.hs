69ab026ae7759ca660fd09859ceb9bfe73237663
Joe Boyle <jboyle617@gmail.com>
2024-06-03 23:26:09 -0400

    Person typeclass

--- src/Typeclasses.hs
+++ src/Typeclasses.hs
@@ -1,4 +1,4 @@
-module Typeclasses where
+module Typeclasses (Shape (..), Point (..), surface, nudge, main) where
 
 data Point = Point Float Float deriving (Show)
 data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
@@ -11,7 +11,24 @@ nudge :: Shape -> Float -> Float -> Shape
 nudge (Circle (Point x y) r) nudgeX nudgeY = Circle (Point (x + nudgeX) (y + nudgeY)) r
 nudge (Rectangle (Point x1 y1) (Point x2 y2)) nudgeX nudgeY = Rectangle (Point (x1 + nudgeX) (y1 + nudgeY)) (Point (x2 + nudgeX) (y2 + nudgeY))
 
+baseCircle = Circle (Point 0 0)
+baseRect width height = Rectangle (Point 0 0) (Point width height)
+
 rect = Rectangle (Point 1 2) (Point 3 4)
 
+-- ew
+-- data Person = Person String String Int Float String String deriving (Show)
+
+-- better! Functions are automatically created for each property, e.g. firstName guy
+data Person = Person { firstName :: String
+                     , lastName :: String
+                     , age :: Int
+                     , height :: Float
+                     , phoneNumber :: String
+                     , favoriteFlavor :: String
+                     } deriving (Show)
+
+guy = Person "George" "Gefferson" 42 3.5 "1-234-567-8910" "Blue Razzleberry"
+
 main :: IO ()
-main = print (nudge rect 1 2)
+main = print guy
